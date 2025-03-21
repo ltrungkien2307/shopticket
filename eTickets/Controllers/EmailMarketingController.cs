4576f0ec43cf34e5dad925185476a94e5555e18f
ltrungkien2307 <ltrungkienn202@gmail.com>
2025-03-21 19:44:29 +0700

    Thêm chức năng Email Marketing

---
+++ eTickets/Controllers/EmailMarketingController.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using eTickets.Models;
+using eTickets.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks; 
+    
+namespace eTickets.Controllers
+{
+    public class EmailMarketingController : Controller
+    {
+        private readonly EmailService _emailService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public EmailMarketingController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public IActionResult SendMarketingEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMarketingEmail(EmailMarketingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            string templatePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Templates/EmailTemplate.html");
+            string link = "https://localhost:44300/EmailMarketing/Unsubscribe";
+
+            string emailContent = await _emailService.LoadEmailTemplateAsync(templatePath, model.Name, link);
+            await _emailService.SendEmailAsync(model.Email, "uu dai dac biet cho ban", emailContent);
+
+            ViewBag.Message = "Email sent successfully";
+            return View();
+
+        }
+    }
+}
---
+++ eTickets/Models/EmailMarketingModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace eTickets.Models;
+
+public class EmailMarketingModel
+{
+    [Required(ErrorMessage = "Please enter your email")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email")]
+
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; }
+}
